4cdd342d7afb5c31cff2a84a4e5cd97a0684ffcd
zimboy <zimand3@gmail.com>
2021-10-15 20:37:37 +0300

    LAB1 commited on branch lab1

---
+++ zdim1981-backend/src/main/java/edu/bbte/idde/zdim1981/backend/Backend.java
@@ -0,0 +1,9 @@
+package edu.bbte.idde.zdim1981.backend;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Backend {
+
+    public static final Logger LOG = LoggerFactory.getLogger(Backend.class);
+}
---
+++ zdim1981-desktop/src/main/java/edu/bbte/idde/zdim1981/desktop/FrontendMain.java
@@ -0,0 +1,32 @@
+package edu.bbte.idde.zdim1981.desktop;
+
+import edu.bbte.idde.zdim1981.backend.Backend;
+import edu.bbte.idde.zdim1981.backend.dataAccessObject.HardwareShopCpuDao;
+import edu.bbte.idde.zdim1981.backend.model.BaseEntity;
+import edu.bbte.idde.zdim1981.backend.model.HardwareShopCpu;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FrontendMain {
+    public static final Backend backend = new Backend();
+    public static final Logger LOG = LoggerFactory.getLogger(FrontendMain.class);
+
+    public static void main(String[] args) {
+        // System.out.println("Hello " + backend.getName());
+        HardwareShopCpuDao cpuDao = new HardwareShopCpuDao();
+        cpuDao.create(new HardwareShopCpu("AMD Ryzen 9 5950X", 2000, 3.4, 1, 16));
+        HardwareShopCpu newCpu = new HardwareShopCpu("Intel Core i9", 1750.6, 3.6, 2, 24);
+
+        LOG.info(cpuDao.read(0L).toString());
+        cpuDao.update(newCpu, 0L);
+        LOG.info(cpuDao.read(0L).toString());
+        cpuDao.delete(0L);
+
+        cpuDao.create(new HardwareShopCpu("AMD Ryzen 9 5950X", 2000, 3.4, 1, 16));
+        cpuDao.create(newCpu);
+
+        for (BaseEntity cpu : cpuDao.readAll()) {
+            LOG.info(cpu.toString());
+        }
+    }
+}
