f32e02703b6f82c4bebd41297f7ddf9068a13441
zimboy <zimand3@gmail.com>
2022-01-09 16:56:07 +0200

    lab5 finalized

--- zdim1981-spring/src/main/java/edu/bbte/idde/zdim1981/dao/jpa/CpuJpaDao.java
+++ zdim1981-spring/src/main/java/edu/bbte/idde/zdim1981/dao/jpa/CpuJpaDao.java
@@ -2,7 +2,11 @@ package edu.bbte.idde.zdim1981.dao.jpa;
 
 import edu.bbte.idde.zdim1981.dao.CpuDao;
 import edu.bbte.idde.zdim1981.model.Cpu;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Profile("jpa")
+@Repository
 public interface CpuJpaDao extends CpuDao, JpaRepository<Cpu, Long> {
 }
--- zdim1981-spring/src/main/java/edu/bbte/idde/zdim1981/dao/jpa/MotherboardJpaDao.java
+++ zdim1981-spring/src/main/java/edu/bbte/idde/zdim1981/dao/jpa/MotherboardJpaDao.java
@@ -2,7 +2,11 @@ package edu.bbte.idde.zdim1981.dao.jpa;
 
 import edu.bbte.idde.zdim1981.dao.MotherboardDao;
 import edu.bbte.idde.zdim1981.model.Motherboard;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Profile("jpa")
+@Repository
 public interface MotherboardJpaDao extends MotherboardDao, JpaRepository<Motherboard, Long> {
 }
