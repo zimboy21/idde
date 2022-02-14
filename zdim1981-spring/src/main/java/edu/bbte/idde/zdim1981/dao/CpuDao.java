d2c78b1c8337759337f4dde6a638ba9ab47c0357
zimboy <zimand3@gmail.com>
2022-02-14 12:14:03 +0200

    first push

--- zdim1981-spring/src/main/java/edu/bbte/idde/zdim1981/dao/CpuDao.java
+++ zdim1981-spring/src/main/java/edu/bbte/idde/zdim1981/dao/CpuDao.java
@@ -1,6 +1,9 @@
 package edu.bbte.idde.zdim1981.dao;
 
 import edu.bbte.idde.zdim1981.model.Cpu;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.Collection;
 
 public interface CpuDao extends Dao<Cpu> {
